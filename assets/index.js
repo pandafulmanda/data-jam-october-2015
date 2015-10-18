102f7ede0f0bb6560fe3b3541edd0c03db1af70b
pandafulmanda <pandafulmanda@gmail.com>
2015-10-17 19:51:55 -0500

    color using brewer theme, make colors more clear, legend in title, fix subdivision drilldown chart

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,14 @@
 var MARKET = 'DAYSONMARKET';
 var CLOSING = 'DAYSTOCLOSING';
+var BINSIZE = 50;
 
 (function(){
   var mainChartElement = document.getElementById("main");
   var mainChartHeading = document.querySelector("[data-chart=main]");
+
   var breakdownChartElement = document.getElementById("breakdown");
+  var breakdownChartHeading = document.querySelector("[data-chart=breakdown]");
+
   var ctx = mainChartElement.getContext("2d");
   var breakdownCtx = breakdownChartElement.getContext("2d");
 
@@ -12,19 +16,21 @@ var CLOSING = 'DAYSTOCLOSING';
 
     var daysOnMarketBySub = groupDaysBySubdivision(data, MARKET);
     var daysToClosingBySub = groupDaysBySubdivision(data, CLOSING);
-    var myBarChart = plotMain();
+    var allDays = _(data).pluck(MARKET).union(_.pluck(data, CLOSING)).value();
+    var dayBins = binBy(allDays, BINSIZE);
+    var mainChart = plotMain();
+    var breakdownChart;
 
     mainChartElement.onclick = function(evt){
-      var activeBars = myBarChart.getBarsAtEvent(evt);
-      var breakdownChart = plotBreakdown(_.first(activeBars).label);
+      var activeBars = mainChart.getBarsAtEvent(evt);
+      breakdownChart = plotBreakdown(_.first(activeBars).label, dayBins);
     };
 
     mainChartHeading.classList.remove('loading');
 
-
     function groupDaysBySubdivision(data, factor){
       return _(data).groupBy(function(house){
-        return house.LEGALSUBDIVISION;
+        return s.titleize(house.LEGALSUBDIVISION);
       })
       .mapValues(function(sub){
         var daysToClosing = _.pluck(sub, factor);
@@ -45,14 +51,20 @@ var CLOSING = 'DAYSTOCLOSING';
       return drawBarChart(ctx, averageDaysOnMarketBySub, averageDaysToClosingBySub);
     }
 
-    function plotBreakdown(subdivision){
+    function plotBreakdown(subdivision, dayBins){
       var marketDaysInBins = binArray(daysOnMarketBySub[subdivision]);
       var closingDaysInBins = binArray(daysToClosingBySub[subdivision]);
+      breakdownChartHeading.dataset.sub = subdivision;
 
-      return drawBarChart(breakdownCtx, marketDaysInBins, closingDaysInBins);
+      if(_.isUndefined(breakdownChart)){
+        return drawBarChart(breakdownCtx, marketDaysInBins, closingDaysInBins);
+      } else {
+        return updateBarChart(breakdownChart, marketDaysInBins, closingDaysInBins);
+      }
 
       function binArray(days){
-        var destination = {};
+        var dayLabels = _.pluck(dayBins, 'label');
+        var destination = _.zipObject(dayLabels, _.fill(new Array(dayLabels.length), 0));
 
         var days = _.sortBy(days, function(day){
           return day * 1;
@@ -63,14 +75,52 @@ var CLOSING = 'DAYSTOCLOSING';
       }
 
       function binTo(day, destination){
-        var twentyRange = Math.floor(day/20);
-        var label = (twentyRange)* 20 + ' - ' + (twentyRange + 1) * 20;
+        var rangeIndex = Math.floor(day / BINSIZE);
+        var label = (rangeIndex)* BINSIZE + ' - ' + (rangeIndex + 1) * BINSIZE;
 
         destination[label] = destination[label]? destination[label] : 0;
 
         destination[label] ++;
       }
     }
+
+    function binBy(array, binSize){
+      var min = _.min(array);
+      var max = _.max(array);
+      var minStart = min -  (binSize - Math.abs(min % binSize));
+      var maxEnd = max + (binSize - max % binSize);
+
+      var range = _.range(minStart, maxEnd, binSize);
+      var bins = [];
+
+      _.each(range, function(rangeBorder, index){
+        var bin = {};
+        if(index == (range.length - 1)){
+          return;
+        }
+        bin.label = rangeBorder + ' - ' + range[index + 1];
+        bin.range = [rangeBorder, range[index + 1]];
+
+        bins.push(bin);
+      });
+
+      return bins;
+    }
+
+    function updateBarChart(barChart){
+      var dataArrays = _.rest(arguments);
+
+      _.each(barChart.datasets, function(dataset, dataIndex){
+        var values = _.values(dataArrays[dataIndex]);
+        _.each(dataset.bars, function(bar, barIndex){
+          bar.value = values[barIndex];
+        });
+      });
+
+      barChart.update();
+      return barChart;
+    }
+
 
     function drawBarChart(chartCtx, dataAsArray, otherDataAsArray){
 
@@ -82,17 +132,17 @@ var CLOSING = 'DAYSTOCLOSING';
         labels: labels,
         datasets: [{
           label: "Days on Market",
-          fillColor: "rgba(151,187,205,0.5)",
-          strokeColor: "rgba(151,187,205,0.8)",
-          highlightFill: "rgba(151,187,205,0.75)",
-          highlightStroke: "rgba(151,187,205,1)",
+          fillColor: "rgba(51,160,44,0.5)",
+          strokeColor: "rgba(51,160,44,0.8)",
+          highlightFill: "rgba(51,160,44,0.75)",
+          highlightStroke: "rgba(51,160,44,1)",
           data: values
         }, {
           label: "Days to Closing",
-          fillColor: "rgba(220,220,220,0.5)",
-          strokeColor: "rgba(220,220,220,0.8)",
-          highlightFill: "rgba(220,220,220,0.75)",
-          highlightStroke: "rgba(220,220,220,1)",
+          fillColor: "rgba(31,120,180,0.5)",
+          strokeColor: "rgba(31,120,180,0.8)",
+          highlightFill: "rgba(31,120,180,0.75)",
+          highlightStroke: "rgba(31,120,180,1)",
           data: secondValues
         }]
       };
